5a15b89097fd65d135d25047dda17a21c1d75585
lucy.bowles <lucy.bowles@brandwatch.com>
2023-01-20 11:39:22 +0000

    using tokens.py for access

--- create_playlist.py
+++ create_playlist.py
@@ -1,16 +1,15 @@
 import requests
+import tokens
 
-USER_ID = "o4rmz7xg2tcjaxrhs93otenmr"
 SPOTIFY_CREATE_PLAYLIST_URL = (
     "https://api.spotify.com/v1/users/o4rmz7xg2tcjaxrhs93otenmr/playlists"
 )
-ACCESS_TOKEN = "BQACs5V8t0MASB_xtgvE4tOVEp3ffALHzOcDpVw1R5N8FrAMCMWg8q9wED5yRzR0IEk98cnngyie3wAtvMAzSKZNy9phNBeJCYsuQ37-sUUDU_IUE5tcqhXsmqdxRJhnVJg90sFSDRLKQTAJj69RlgSWEQiHU4uY0UthYD9cVLvSZz6s-2PftqcWrVwx-he7pHQSEzj3cVnr7uKgbgsuwXLml0JXfmIqsyobDAMgnHYgQdp_stXzdGW9pFyv4WpA"
 
 
 def create_playlist_on_spotify(name, public):
     response = requests.post(
         SPOTIFY_CREATE_PLAYLIST_URL,
-        headers={"Authorization": f"Bearer {ACCESS_TOKEN}"},
+        headers={"Authorization": f"Bearer {tokens.ACCESS_TOKEN}"},
         json={"name": name, "public": public},
     )
     json_resp = response.json()
